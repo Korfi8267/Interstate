27204357e70754444c56315fcb2594d81dc3d99d
Korfi8267 <macwells3599@yahoo.com>
2022-10-20 21:30:22 -0500

    Fix animation bug + partial scrolling fix

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,5 +1,5 @@
 <template>
-<nav class="sm:hidden bg-gray-100 dark:bg-slate-800 sticky top-0">
+<nav class="sm:hidden bg-gray-100 dark:bg-slate-800 shadow-md overscroll-auto w-full sticky z-20">
   <div class="max-w-7xl mx-auto px-2">
     <div class="justify-center items-center p-5 gap-8 flex flex-row">
       <img class="justify-left rounded-full w-8 h-8" src="../assets/mii2.png" />
@@ -72,7 +72,7 @@
     </div>
 </nav>
 
-<nav class="hidden sm:block bg-gray-100 dark:bg-slate-800 sticky top-0">
+<nav class="hidden sm:block bg-gray-100 dark:bg-slate-800 shadow-md overscroll-x-none overscroll-y-auto fixed w-screen z-50">
   <div class="max-w-7xl mx-auto px-2">
     <div class="justify-left items-center p-5 gap-8 flex flex-row">
       <img class="rounded-full w-8 h-8" src="../assets/mii2.png" />
