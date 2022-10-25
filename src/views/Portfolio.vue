ffb8efd57c141202c696750ce684a28de261d228
Korfi8267 <macwells3599@yahoo.com>
2022-10-24 21:39:15 -0500

    swap portfolio content for packages content

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,127 +1,20 @@
 <template>
-<div class="w-screen h-screen overflow-auto">
+<div class="w-screen h-screen overflow-auto scroll-smooth">
 <div class="my-32"></div>
 
+
 <div class="text-center">
-  <img class="mx-auto align-center rounded-full w-32 h-32 transition ease-in-out delay-50 bg-blue-100 hover:bg-blue-200 dark:bg-blue-900 dark:hover:bg-blue-800" src="../assets/mii2.png" />
+  <img class="mx-auto align-center rounded-full w-32 h-32 transition ease-in-out bg-blue-100 hover:bg-blue-200 dark:bg-blue-900 dark:hover:bg-blue-800" src="../assets/mii2.png" />
   <div class="my-5"></div>
   <span class="text-5xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">You're... Early.</span>
   <div class="my-4"></div>
   <span class="text-xl font-medium text-black dark:text-white">This page isn't done yet. Check back later.</span>
 </div><br/>
 
-<div class="my-16"></div>
-
-<!-- Carousel -->
-<div class="text-center font-regular text-blue-600 dark:text-blue-300">
-  <span class="text-2xl">Carousel Here!</span><br/><span class="text-xl text-slate-500 dark:text-white">Showcases featured items.</span>
-</div>
-
-<div class="my-16"></div>
-
-<!-- Latest Item -->
-<div class="flex flex-col items-center justify-center px-5">
-<div class="grow"></div>
-  <div class="flex-none box-border h-auto w-full max-w-6xl rounded-lg shadow-lg bg-slate-100 dark:bg-slate-800 p-8 overflow-auto">
-    <div class="flex flex-wrap flex-row justify-left gap-3">
-      <span class="flex-none inline-block px-3 py-0.5 box-border h-auto w-auto rounded-full bg-red-500 text-md text-white font-semibold">Tag</span> 
-      <span class="grow inline-block font-semibold text-black dark:text-white text-xl">Title Header</span>
-      <span class="hidden md:block flex-none px-3 font-regular text-gray-500 dark:text-gray-400 text-xl">Date added</span>
-    </div>
-    <div class="my-2"></div>
-      <span class="inline-block font-regular text-black dark:text-gray-400 text-lg">This box will host a new announcement regarding a project, such as a new theme or a concept. It'll showcase a custom tag, a title, the publication date, and a quick description.</span>
-    </div>
-    <div class="grow"></div>
-</div>
-<br/>
-
-<div class="my-16"></div>
-
-<!-- Item Grid -->
-<div class="flex flex-row flex-wrap gap-x-10 gap-y-7 px-6 justify-center overflow-hidden">
-<div class="flex flex-col lg:flex-row flex-wrap gap-y-7 gap-x-10 justify-center">
-  <div class="bg-[url('../assets/greengrass2.jpeg')] relative bg-cover bg-center text-left box-border h-64 w-64 rounded-lg shadow-lg bg-slate-100 dark:bg-slate-800">
-    <div class="absolute bottom-1 left-1 right-1 flex flex-row">
-    <span class="flex-none rounded-md py-1.5 px-3 bg-gray-700/50 text-white font-semibold backdrop-blur-sm">Green Grass</span>
-    <span class="grow"></span>
-    <div class="rounded-md py-1.5 px-2 flex-none transition ease-in-out delay-50 hover:scale-105 bg-gray-700/50 text-white backdrop-blur-sm hover:text-black hover:bg-white/50">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 h-5 mt-0.5">
-        <path fill="currentColor" d="M504 256c0 137-111 248-248 248S8 393 8 256 119 8 256 8s248 111 248 248zM212 140v116h-70.9c-10.7 0-16.1 13-8.5 20.5l114.9 114.3c4.7 4.7 12.2 4.7 16.9 0l114.9-114.3c7.6-7.6 2.2-20.5-8.5-20.5H300V140c0-6.6-5.4-12-12-12h-64c-6.6 0-12 5.4-12 12z"/>
-      </svg>
-    </div>
-  </div>
-</div>
-
-<div class="bg-[url('../assets/greengrass.jpeg')] relative bg-cover bg-center text-left box-border h-64 w-64 rounded-lg shadow-lg bg-slate-100 dark:bg-slate-800">
-  <div class="absolute bottom-1 left-1 right-1 flex flex-row">
-    <span class="flex-none rounded-md py-1.5 px-3 bg-gray-700/50 text-white font-semibold backdrop-blur-sm">Greener Grass</span>
-    <span class="grow"></span>
-    <div class="rounded-md py-1.5 px-2 flex-none transition ease-in-out delay-50 hover:scale-105 bg-gray-700/50 text-white backdrop-blur-sm hover:text-black hover:bg-white/50">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 h-5 mt-0.5">
-      <path fill="currentColor" d="M504 256c0 137-111 248-248 248S8 393 8 256 119 8 256 8s248 111 248 248zM212 140v116h-70.9c-10.7 0-16.1 13-8.5 20.5l114.9 114.3c4.7 4.7 12.2 4.7 16.9 0l114.9-114.3c7.6-7.6 2.2-20.5-8.5-20.5H300V140c0-6.6-5.4-12-12-12h-64c-6.6 0-12 5.4-12 12z"/>
-      </svg>
-    </div>
-  </div>
-</div>
-
-<div class="bg-[url('../assets/sunset.jpeg')] relative bg-cover bg-center text-left box-border h-64 w-64 rounded-lg shadow-lg bg-slate-100 dark:bg-slate-800">
-  <div class="absolute bottom-1 left-1 right-1 flex flex-row">
-    <span class="flex-none rounded-md py-1.5 px-3 bg-gray-700/50 text-white font-semibold backdrop-blur-sm">Mountain Sunset</span>
-    <span class="grow"></span>
-    <div class="rounded-md py-1.5 px-2 flex-none transition ease-in-out delay-50 hover:scale-105 bg-gray-700/50 text-white backdrop-blur-sm hover:text-black hover:bg-white/50">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 h-5 mt-0.5">
-      <path fill="currentColor" d="M504 256c0 137-111 248-248 248S8 393 8 256 119 8 256 8s248 111 248 248zM212 140v116h-70.9c-10.7 0-16.1 13-8.5 20.5l114.9 114.3c4.7 4.7 12.2 4.7 16.9 0l114.9-114.3c7.6-7.6 2.2-20.5-8.5-20.5H300V140c0-6.6-5.4-12-12-12h-64c-6.6 0-12 5.4-12 12z"/>
-      </svg>
-    </div>
-  </div>
-</div>
-
-  </div>
-  <div class="flex flex-col lg:flex-row flex-wrap gap-y-7 gap-x-10 justify-center">
-    <div class="bg-[url('../assets/sunset.jpeg')] relative bg-cover bg-center text-left box-border h-64 w-64 rounded-lg shadow-lg bg-slate-100 dark:bg-slate-800">
-  <div class="absolute bottom-1 left-1 right-1 flex flex-row">
-    <span class="flex-none rounded-md py-1.5 px-3 bg-gray-700/50 text-white font-semibold backdrop-blur-sm">Mountain Sunset</span>
-    <span class="grow"></span>
-    <div class="rounded-md py-1.5 px-2 flex-none transition ease-in-out delay-50 hover:scale-105 bg-gray-700/50 text-white backdrop-blur-sm hover:text-black hover:bg-white/50">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 h-5 mt-0.5">
-      <path fill="currentColor" d="M504 256c0 137-111 248-248 248S8 393 8 256 119 8 256 8s248 111 248 248zM212 140v116h-70.9c-10.7 0-16.1 13-8.5 20.5l114.9 114.3c4.7 4.7 12.2 4.7 16.9 0l114.9-114.3c7.6-7.6 2.2-20.5-8.5-20.5H300V140c0-6.6-5.4-12-12-12h-64c-6.6 0-12 5.4-12 12z"/>
-      </svg>
-    </div>
-  </div>
-</div>
-
-<div class="bg-[url('../assets/greengrass2.jpeg')] relative bg-cover bg-center text-left box-border h-64 w-64 rounded-lg shadow-lg bg-slate-100 dark:bg-slate-800">
-  <div class="absolute bottom-1 left-1 right-1 flex flex-row">
-    <span class="flex-none rounded-md py-1.5 px-3 bg-gray-700/50 text-white font-semibold backdrop-blur-sm">Green Grass</span>
-    <span class="grow"></span>
-    <div class="rounded-md py-1.5 px-2 flex-none transition ease-in-out delay-50 hover:scale-105 bg-gray-700/50 text-white backdrop-blur-sm hover:text-black hover:bg-white/50">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 h-5 mt-0.5">
-      <path fill="currentColor" d="M504 256c0 137-111 248-248 248S8 393 8 256 119 8 256 8s248 111 248 248zM212 140v116h-70.9c-10.7 0-16.1 13-8.5 20.5l114.9 114.3c4.7 4.7 12.2 4.7 16.9 0l114.9-114.3c7.6-7.6 2.2-20.5-8.5-20.5H300V140c0-6.6-5.4-12-12-12h-64c-6.6 0-12 5.4-12 12z"/>
-      </svg>
-    </div>
-  </div>
-</div>
-
-<div class="bg-[url('../assets/greengrass.jpeg')] relative bg-cover bg-center text-left box-border h-64 w-64 rounded-lg shadow-lg bg-slate-100 dark:bg-slate-800">
-  <div class="absolute bottom-1 left-1 right-1 flex flex-row">
-    <span class="flex-none rounded-md py-1.5 px-3 bg-gray-700/50 text-white font-semibold backdrop-blur-sm">Greener Grass</span>
-    <span class="grow"></span>
-    <div class="rounded-md py-1.5 px-2 flex-none transition ease-in-out delay-50 hover:scale-105 bg-gray-700/50 text-white backdrop-blur-sm hover:text-black hover:bg-white/50">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 h-5 mt-0.5">
-      <path fill="currentColor" d="M504 256c0 137-111 248-248 248S8 393 8 256 119 8 256 8s248 111 248 248zM212 140v116h-70.9c-10.7 0-16.1 13-8.5 20.5l114.9 114.3c4.7 4.7 12.2 4.7 16.9 0l114.9-114.3c7.6-7.6 2.2-20.5-8.5-20.5H300V140c0-6.6-5.4-12-12-12h-64c-6.6 0-12 5.4-12 12z"/>
-      </svg>
-    </div>
-  </div>
-</div>
-  </div>
-</div><br/>
-<div class="text-center text-slate-700 dark:text-slate-300 text-lg font-regular">This will often be updated with new content.</div>
-<div class="text-center text-blue-300 text-lg font-medium">Check back every now and then.</div>
-
-<div class="my-96"></div>
+<div class="my-12"></div>
 
 <div class="text-center font-regular text-blue-600 dark:text-blue-300">
-  <span class="text-lg">Designed by <a class="font-medium text-slate-500 dark:text-white">Korfi</a> in Oklahoma</span>
+  <span class="text-md">Designed by <a class="font-medium text-slate-500 dark:text-white">Korfi</a> in Oklahoma</span>
 </div>
 
 </div>
