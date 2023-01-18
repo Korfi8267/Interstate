abc140254e7b66a72feb3bc134920d93b04cb710
Korfi8267 <macwells3599@yahoo.com>
2023-01-18 00:03:15 -0600

    test video player

--- src/views/Packages.vue
+++ src/views/Packages.vue
@@ -124,6 +124,9 @@
 </div>
 <div class="my-12"></div>
 
+<iframe width="560" height="315" src="https://www.youtube.com/embed/0VxoWT0MyLE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+
 </div>
 </template>
 <script>
