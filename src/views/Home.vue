fb16f433a70487ad1fc8d8c2d1ac748afa6fe103
Korfi8267 <macwells3599@yahoo.com>
2023-01-18 00:00:05 -0600

    complete home page redesign

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,49 +2,90 @@
 <div class="w-screen h-screen overflow-auto scroll-smooth">
 <div class="my-32"></div>
 
-<div class="text-center">
-  <img class="mx-auto align-center rounded-full w-32 h-32 transition ease-in-out delay-50 bg-blue-100 hover:bg-blue-200 dark:bg-blue-900 dark:hover:bg-blue-800" src="../assets/mii2.png" />
-  <div class="my-5"></div>
-  <span class="text-5xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">Hello.</span>
-  <div class="my-4"></div>
-  <span class="text-xl font-medium text-black dark:text-white">I'm a designer and photographer at heart.</span>
-</div><br/>
-
-
-<div class="justify-center px-6 flex flex-row flex-wrap gap-6">
-  <div class="box-border h-10 w-40 transition ease-in-out delay-50 hover:scale-105 bg-slate-100 dark:bg-slate-700 hover:bg-slate-200 dark:hover:bg-slate-600 hover:shadow-lg rounded-md">
-    <button class="transition ease-in-out delay-50 text-black dark:text-white hover:text-blue-600 dark:hover:text-blue-300 mx-auto px-5 h-10 w-40"><router-link to="/Portfolio">View my work</router-link></button>
+<div class="flex flex-col md:flex-row items-center text-center md:text-left justify-center px-7 md:px-20">
+  <img class="w-16 md:w-12 h-16 md:h-12 transition ease-in-out delay-50 hover:rotate-12" src="../assets/handwave.png" /><div class="px-4 py-4"></div>
+  <div class="text-3xl font-semibold text-black dark:text-white">Hi there.<div class="text-xl font-normal text-blue-500 dark:text-blue-300">I'm a designer and photographer.</div></div>
+  <div class="py-4 md:py-0"></div><div class="flex grow max-w-[700px]"></div>
+  <div class="box-border h-10 w-40 transition ease-in-out delay-50 hover:scale-105 bg-slate-100 dark:bg-slate-700 hover:bg-blue-400 dark:hover:bg-blue-600 rounded-md">
+    <button class="transition ease-in-out delay-50 text-black dark:text-white hover:text-gray-100 mx-auto px-5 h-10 w-40"><router-link to="/Portfolio">View my work</router-link></button>
   </div>
-
-  <div class="box-border h-10 w-40 transition ease-in-out delay-50 hover:scale-105 bg-slate-100 dark:bg-slate-700 hover:bg-slate-200 dark:hover:bg-slate-600 hover:shadow-lg rounded-md">
-    <button class="transition ease-in-out delay-50 text-black dark:text-white hover:text-blue-600 dark:hover:text-blue-300 mx-auto px-5 h-10 w-40"><router-link to="/Contact">Contact Me</router-link></button>
-  </div>
-</div><br/>
-
-<div class="my-12"></div>
-
-<div class="justify-center px-6 flex items-center flex-col md:flex-row flex-wrap gap-6">
-  <div class="box-border h-auto w-[370px] md:w-[330px] xl:w-[370px] rounded-lg shadow-lg bg-slate-100 dark:bg-slate-800 p-8 overflow-auto">
-    <span class="inline-block font-semibold text-black dark:text-white text-xl align-middle">About Me</span><br/>
-    <span class="inline-block font-regular text-gray-500 dark:text-gray-400 text-lg">I am a self-taught designer and photographer. My favorite hobbies include stargazing and playing video games.</span>
-  </div>
-
-  <div class="box-border h-auto w-[370px] md:w-[330px] xl:w-[370px] rounded-lg shadow-lg bg-slate-100 dark:bg-slate-800 p-8 overflow-auto">
-    <span class="inline-block font-semibold text-black dark:text-white text-xl align-middle">My Work</span><br/>
-    <span class="inline-block font-regular text-gray-500 dark:text-gray-400 text-lg">My work ranges from software designs such as Ontario, to icon packs such as iOffice, to a variety of photos I take.</span>
-  </div>
-
-  <div class="box-border h-auto w-[370px] md:w-[684px] xl:w-[370px] rounded-lg shadow-lg bg-slate-100 dark:bg-slate-800 p-8">
-    <span class="inline-block font-semibold text-black dark:text-white text-xl align-middle">My Plans</span><br/>
-    <span class="inline-block font-regular text-gray-500 dark:text-gray-400 text-lg">I plan to go to a 2 year Multimedia class part time to learn more about graphical / web design and photography.</span>
-  </div>
-</div><br/>
-
-<div class="my-12"></div>
-
-<div class="text-center font-regular text-blue-600 dark:text-blue-300">
-  <span class="text-lg">Designed by <a class="font-medium text-slate-500 dark:text-white">Korfi</a> in Oklahoma</span>
 </div>
+
+<div class="my-12 md:my-24"/>
+
+<div class="flex flex-row items-center justify-center px-7 md:px-20">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-black dark:text-white">
+  <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clip-rule="evenodd" />
+</svg>
+  <div class="mx-1"/>
+  <div class="flex-grow max-w-[1199px] text-2xl font-semibold text-black dark:text-white">
+    About Me
+  </div>
+</div>
+<div class="flex flex-row px-7 md:px-20 justify-center">
+  <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-slate-400">
+      I am Mac, otherwise known as Korfi online.
+      <div class="my-3"/> 
+      I am a graphic designer and amatuer photographer from Oklahoma. I am attending a 
+      school for a Multimedia class where I am learning image editing, coding, graphical 
+      design and more. 
+      <div class="my-3"/>
+      I have roughly <a class="text-gray-800 dark:text-slate-300 font-semibold">2 years</a> 
+      of experience designing software concepts. I alays love to tinker with design, as
+      well as experiment with new ideas pushing the limits of the software we have today.
+    </div>
+</div>
+
+<div class="my-10"/>
+
+<div class="flex flex-row items-center justify-center px-7 md:px-20">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-black dark:text-white">
+  <path d="M3.375 3C2.339 3 1.5 3.84 1.5 4.875v.75c0 1.036.84 1.875 1.875 1.875h17.25c1.035 0 1.875-.84 1.875-1.875v-.75C22.5 3.839 21.66 3 20.625 3H3.375z" />
+  <path fill-rule="evenodd" d="M3.087 9l.54 9.176A3 3 0 006.62 21h10.757a3 3 0 002.995-2.824L20.913 9H3.087zm6.163 3.75A.75.75 0 0110 12h4a.75.75 0 010 1.5h-4a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
+</svg>
+  <div class="mx-1"/>
+  <div class="flex-grow max-w-[1199px] text-2xl font-semibold text-black dark:text-white">My Projects
+  </div>
+</div>
+<div class="flex flex-row px-7 md:px-20 justify-center">
+  <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-slate-400">
+      My most notable project is <a class="font-semibold text-black dark:text-slate-300">iOffice</a>.
+      <div class="my-3"/>
+      However I have created a multitude of other projects, usually revolving around designs
+      and concepts. Most revolve around Apple's iOS, iPadOS and macOS softwares respectively.
+      <div class="my-3"/>
+      I have also collaborated with a multitude of designers and developers, including 
+      <a class="font-semibold text-gray-800 dark:text-slate-300">llsc12, Paras KCD, and more.</a>
+      Special thanks to everyone who helped get me this far!
+    </div>
+</div>
+
+<div class="flex px-7 md:px-20">
+<hr class="flex-grow max-w-[1230px] h-1 mx-auto bg-slate-200 border-0 rounded my-10 dark:bg-slate-700">
+</div>
+
+<div class="flex flex-row items-center justify-center px-7 md:px-20">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-black dark:text-white">
+  <path fill-rule="evenodd" d="M4.848 2.771A49.144 49.144 0 0112 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 01-3.476.383.39.39 0 00-.297.17l-2.755 4.133a.75.75 0 01-1.248 0l-2.755-4.133a.39.39 0 00-.297-.17 48.9 48.9 0 01-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97z" clip-rule="evenodd" />
+</svg>
+  <div class="mx-1"/>
+  <div class="flex-grow max-w-[1199px] text-2xl font-semibold text-black dark:text-white">My Contacts
+  </div>
+</div>
+<div class="flex flex-row px-7 md:px-20 justify-center">
+  <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-slate-400">
+      Below you can access any method of contact I have added thus far. These may change often, 
+      so please check every now and then. Please choose from the options available depending on
+      your reason of contact.
+    </div>
+</div>
+
+<div class="my-24"/>
+
+<div class="text-center font-regular text-blue-400 dark:text-blue-300">
+  <span class="text-lg">Created by <a class="font-medium text-blue-600 dark:text-white">Korfi</a> in Oklahoma</span>
+</div>
+
 <div class="my-12"></div>
 
 </div>
