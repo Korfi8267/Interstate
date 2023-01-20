3c386fd709eca4c8edcb3fb5781591573752a3dc
Korfi8267 <macwells3599@yahoo.com>
2023-01-20 00:11:43 -0600

    erase old portfolio code + fix animation bug

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,66 +1,10 @@
 <template>
-<div class="w-screen h-screen overflow-auto scroll-smooth">
-<div class="my-32"></div>
-
-
-<div class="text-center">
-  <img class="mx-auto align-center rounded-full w-32 h-32 transition ease-in-out bg-blue-100 hover:bg-blue-200 dark:bg-blue-900 dark:hover:bg-blue-800" src="../assets/mii2.png" />
-  <div class="my-5"></div>
-  <span class="text-5xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">You're... Early.</span>
-  <div class="my-4"></div>
-  <span class="text-xl font-medium text-black dark:text-white">This page isn't done yet. Check back later.</span>
-</div><br/>
-
-  <div class="text-xl font-semibold text-black dark:text-white">Q: Why are the images in the JPEG format?</div>
-</div><br/>
-<div class="text-xl font-medium text-slate-700 dark:text-slate-300">This is because JPEG does not reduce the quality substantionally, while also keeping the file size at a reasonable limit.</div>
-<div class="my-12">
-
-<div class="flex flex-row flex-nowrap gap-y-7 px-5 gap-x-5 justify-left xl:justify-center overflow-x-auto snap-x">
-  <div class="scroll-pl-6 flex flex-row flex-nowrap gap-x-6 gap-y-7 justify-center items-center">
-  <div class="snap-center bg-[url('../assets/greengrass.jpeg')] relative bg-cover bg-center box-border h-[240px] w-[370px] rounded-lg shadow-lg bg-slate-100 dark:bg-slate-800 p-8">
-    <div class="absolute bottom-1 left-1 right-1 flex flex-row">
-      <span class="flex-none rounded-md py-1.5 px-3 bg-gray-700/50 text-white font-semibold backdrop-blur-sm">Green Grass</span>
-      <span class="grow"></span>
-      <div class="rounded-md py-1.5 px-2 flex-none transition ease-in-out delay-50 hover:scale-105 bg-gray-700/50 text-white backdrop-blur-sm hover:text-black hover:bg-white/50">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 h-5 mt-0.5">
-        <path fill="currentColor" d="M504 256c0 137-111 248-248 248S8 393 8 256 119 8 256 8s248 111 248 248zM212 140v116h-70.9c-10.7 0-16.1 13-8.5 20.5l114.9 114.3c4.7 4.7 12.2 4.7 16.9 0l114.9-114.3c7.6-7.6 2.2-20.5-8.5-20.5H300V140c0-6.6-5.4-12-12-12h-64c-6.6 0-12 5.4-12 12z"/>
-        </svg>
-      </div>
-    </div>
-  </div>
-
-  <div class="snap-center bg-[url('../assets/greengrass2.jpeg')] relative bg-cover bg-center box-border h-[240px] w-[370px] rounded-lg shadow-lg bg-slate-100 dark:bg-slate-800 p-8">
-    <div class="absolute bottom-1 left-1 right-1 flex flex-row">
-      <span class="flex-none rounded-md py-1.5 px-3 bg-gray-700/50 text-white font-semibold backdrop-blur-sm">Green Grass</span>
-      <span class="grow"></span>
-      <div class="rounded-md py-1.5 px-2 flex-none transition ease-in-out delay-50 hover:scale-105 bg-gray-700/50 text-white backdrop-blur-sm hover:text-black hover:bg-white/50">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 h-5 mt-0.5">
-        <path fill="currentColor" d="M504 256c0 137-111 248-248 248S8 393 8 256 119 8 256 8s248 111 248 248zM212 140v116h-70.9c-10.7 0-16.1 13-8.5 20.5l114.9 114.3c4.7 4.7 12.2 4.7 16.9 0l114.9-114.3c7.6-7.6 2.2-20.5-8.5-20.5H300V140c0-6.6-5.4-12-12-12h-64c-6.6 0-12 5.4-12 12z"/>
-        </svg>
-      </div>
-    </div>
-  </div>
-
-  <div class="snap-center bg-[url('../assets/sunset.jpeg')] relative bg-cover bg-center box-border h-[240px] w-[370px] rounded-lg shadow-lg bg-slate-100 dark:bg-slate-800 p-8">
-    <div class="absolute bottom-1 left-1 right-1 flex flex-row">
-      <span class="flex-none rounded-md py-1.5 px-3 bg-gray-700/50 text-white font-semibold backdrop-blur-sm">Green Grass</span>
-      <span class="grow"></span>
-      <div class="rounded-md py-1.5 px-2 flex-none transition ease-in-out delay-50 hover:scale-105 bg-gray-700/50 text-white backdrop-blur-sm hover:text-black hover:bg-white/50">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 h-5 mt-0.5">
-        <path fill="currentColor" d="M504 256c0 137-111 248-248 248S8 393 8 256 119 8 256 8s248 111 248 248zM212 140v116h-70.9c-10.7 0-16.1 13-8.5 20.5l114.9 114.3c4.7 4.7 12.2 4.7 16.9 0l114.9-114.3c7.6-7.6 2.2-20.5-8.5-20.5H300V140c0-6.6-5.4-12-12-12h-64c-6.6 0-12 5.4-12 12z"/>
-        </svg>
-      </div>
-    </div>
-  </div>
-  </div>
-</div><br/>
-
-<div class="text-center font-regular text-blue-600 dark:text-blue-300">
-  <span class="text-md">Designed by <a class="font-medium text-slate-500 dark:text-white">Korfi</a> in Oklahoma</span>
-</div>
-<div class="my-16"></div>
-
+<div class="bg-white dark:bg-slate-900 scroll-smooth overflow-auto">
+  <div class="my-12"/>
+<div class="text-center font-bold text-5xl text-black dark:text-white my-2">You're early.</div>
+<div class="text-center font-medium text-3xl text-slate-700 dark:text-slate-400 mb-16">Check back later.</div>
+<div class="text-center font-medium text-3xl text-slate-700 dark:text-slate-400 mb-2">Here's a Mii.</div>
+<img class="mx-auto align-center rounded-full w-32 h-32 transition ease-in-out bg-blue-100 hover:bg-blue-200 dark:bg-blue-900 dark:hover:bg-blue-800" src="../assets/mii2.png" />
 </div>
 </template>
 <script>
