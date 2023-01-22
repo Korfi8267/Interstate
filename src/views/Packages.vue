5eea8c61a2b17197a1526cb2ab62613c04592df6
Korfi8267 <macwells3599@yahoo.com>
2023-01-21 21:38:06 -0600

    add horizontal card menu template

--- src/views/Packages.vue
+++ src/views/Packages.vue
@@ -1,10 +1,52 @@
 <template>
 <div class="bg-white dark:bg-slate-900 scroll-smooth overflow-auto">
-  <div class="my-12"/>
+<div class="my-12"/>
+
 <div class="text-center font-bold text-5xl text-black dark:text-white my-2">You're early.</div>
 <div class="text-center font-medium text-3xl text-slate-700 dark:text-slate-400 mb-16">Check back later.</div>
 <div class="text-center font-medium text-3xl text-slate-700 dark:text-slate-400 mb-2">Here's a Mii.</div>
-<img class="mx-auto align-center rounded-full w-32 h-32 transition ease-in-out bg-blue-100 hover:bg-blue-200 dark:bg-blue-900 dark:hover:bg-blue-800" src="../assets/mii2.png" />
+<img class="mx-auto align-center rounded-full w-32 h-32 transition ease-in-out bg-blue-100 hover:bg-blue-200 dark:bg-blue-900 dark:hover:bg-blue-800 mb-12" src="../assets/mii2.png" />
+
+<div class="flex flex-nowrap justify-center">
+  <div class="w-[1230px] h-96 flex-row flex-nowrap flex snap-mandatory snap-x scroll-smooth overflow-x-scroll gap-14 px-7 md:px-20 mb-12">
+    <div class="scroll-ml-7 snap-start w-72 h-96 bg-blue-500 rounded-md p-3 flex flex-row shrink-0 items-end">
+      <div class="w-32 h-10 bg-green-500 rounded-md text-white font-semibold">Title</div>
+      <div class="flex-grow"></div>
+      <div class="w-10 h-10 bg-red-500 rounded-md text-white font-semibold">DL</div>
+    </div>
+
+    <div class="scroll-ml-7 snap-start w-72 h-96 bg-blue-500 rounded-md p-3 flex flex-row shrink-0 items-end">
+      <div class="w-32 h-10 bg-green-500 rounded-md text-white font-semibold">Title</div>
+      <div class="flex-grow"></div>
+      <div class="w-10 h-10 bg-red-500 rounded-md text-white font-semibold">DL</div>
+    </div>
+
+    <div class="scroll-ml-7 snap-start w-72 h-96 bg-blue-500 rounded-md p-3 flex flex-row shrink-0 items-end">
+      <div class="w-32 h-10 bg-green-500 rounded-md text-white font-semibold">Title</div>
+      <div class="flex-grow"></div>
+      <div class="w-10 h-10 bg-red-500 rounded-md text-white font-semibold">DL</div>
+    </div>
+
+    <div class="scroll-ml-7 snap-start w-72 h-96 bg-blue-500 rounded-md p-3 flex flex-row shrink-0 items-end">
+      <div class="w-32 h-10 bg-green-500 rounded-md text-white font-semibold">Title</div>
+      <div class="flex-grow"></div>
+      <div class="w-10 h-10 bg-red-500 rounded-md text-white font-semibold">DL</div>
+    </div>
+
+    <div class="scroll-ml-7 snap-start w-72 h-96 bg-blue-500 rounded-md p-3 flex flex-row shrink-0 items-end">
+      <div class="w-32 h-10 bg-green-500 rounded-md text-white font-semibold">Title</div>
+      <div class="flex-grow"></div>
+      <div class="w-10 h-10 bg-red-500 rounded-md text-white font-semibold">DL</div>
+    </div>
+
+    <div class="scroll-ml-7 snap-start w-72 h-96 bg-blue-500 rounded-md p-3 flex flex-row shrink-0 items-end">
+      <div class="w-32 h-10 bg-green-500 rounded-md text-white font-semibold">Title</div>
+      <div class="flex-grow"></div>
+      <div class="w-10 h-10 bg-red-500 rounded-md text-white font-semibold">DL</div>
+    </div>
+  </div>
+</div>
+
 </div>
 </template>
 <script>
