5860fda905beafcd5d850075f38676c35898a50e
Korfi8267 <macwells3599@yahoo.com>
2023-03-19 13:44:07 -0500

    Begin portfolio website design template

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,10 +1,34 @@
 <template>
 <div class="bg-white dark:bg-slate-900 scroll-smooth overflow-auto">
-  <div class="my-12"/>
-<div class="text-center font-bold text-5xl text-black dark:text-white my-2">You're early.</div>
-<div class="text-center font-medium text-3xl text-slate-700 dark:text-slate-400 mb-16">Check back later.</div>
-<div class="text-center font-medium text-3xl text-slate-700 dark:text-slate-400 mb-2">Here's a Mii.</div>
-<img class="mx-auto align-center rounded-full w-32 h-32 transition ease-in-out bg-blue-100 hover:bg-blue-200 dark:bg-blue-900 dark:hover:bg-blue-800" src="../assets/mii2.png" />
+  <div class="my-24"/>
+
+  <div class="flex flex-col md:flex-row items-center text-center md:text-left justify-center px-7 md:px-20">
+    <img class="w-16 md:w-12 h-16 md:h-12" src="../assets/box.png" /><div class="px-4 py-4"></div>
+    <div class="text-3xl font-semibold text-black dark:text-white">My Portfolio.<div class="text-xl font-normal text-gray-600 dark:text-gray-300">My experience of working with what I love.</div></div>
+    <div class="py-4 md:py-0"></div><div class="flex grow max-w-[765px]"></div>
+  </div>
+
+<div class="my-12 md:my-24"/>
+
+<div class="flex flex-row items-center justify-center px-7 md:px-20">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-black dark:text-white">
+  <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clip-rule="evenodd" />
+</svg>
+  <div class="mx-1"/>
+  <div class="flex-grow max-w-[1199px] text-2xl font-semibold text-black dark:text-white">
+    Header
+  </div>
+</div>
+<div class="flex flex-row px-7 md:px-20 justify-center">
+  <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-slate-400">
+      Paragraph 1. Summary of content.
+      <div class="my-3"/> 
+      Paragraph 2. Brief explanation of content. 
+      <div class="my-3"/>
+      Paragraph 3. <a class="text-gray-800 dark:text-slate-300 font-semibold">Important info.</a> 
+      Quick conclusion of explanation of content.
+    </div>
+</div>
 </div>
 </template>
 <script>
