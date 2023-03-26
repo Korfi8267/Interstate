49a6b0e3900f2f275858860169d321060203754e
Korfi8267 <macwells3599@yahoo.com>
2023-03-26 12:12:41 -0500

    CHG: Subheader, ADD: portfilio information

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -3,46 +3,126 @@
   <div class="my-24"/>
 
   <div class="flex flex-col md:flex-row items-center text-center md:text-left justify-center px-7 md:px-20">
-    <img class="w-16 md:w-12 h-16 md:h-12" src="../assets/box.png" /><div class="px-4 py-4"></div>
-    <div class="text-3xl font-semibold text-black dark:text-white">My Portfolio.<div class="text-xl font-normal text-gray-600 dark:text-gray-300">My experience of working with what I love.</div></div>
-    <div class="py-4 md:py-0"></div><div class="flex grow max-w-[780px]"></div>
+    <img class="w-16 md:w-12 h-16 md:h-12" src="../assets/binder.png" /><div class="px-4 py-4"></div>
+    <div class="text-3xl font-semibold text-black dark:text-white">My Portfolio.<div class="text-xl font-normal text-gray-600 dark:text-gray-300">My experience and contributions.</div></div>
+    <div class="py-4 md:py-0"></div><div class="flex grow max-w-[858px]"></div>
   </div>
 
-<div class="my-12 md:my-24"/>
+<div class="my-[88px] md:my-24"/>
 
 <div class="flex flex-row items-center justify-center px-7 md:px-20">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-black dark:text-white">
-  <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clip-rule="evenodd" />
+  <path d="M3.375 3C2.339 3 1.5 3.84 1.5 4.875v.75c0 1.036.84 1.875 1.875 1.875h17.25c1.035 0 1.875-.84 1.875-1.875v-.75C22.5 3.839 21.66 3 20.625 3H3.375z" />
+  <path fill-rule="evenodd" d="M3.087 9l.54 9.176A3 3 0 006.62 21h10.757a3 3 0 002.995-2.824L20.913 9H3.087zm6.163 3.75A.75.75 0 0110 12h4a.75.75 0 010 1.5h-4a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
 </svg>
   <div class="mx-1"/>
-  <div class="flex-grow max-w-[1199px] text-2xl font-semibold text-black dark:text-white">
-    iOffice
+  <div class="flex-grow max-w-[1199px] text-2xl font-semibold text-black dark:text-white">Photo Editing
   </div>
 </div>
 <div class="flex flex-row px-7 md:px-20 justify-center">
-  <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-slate-400 mb-4">
-      iOffice is an icon pack that resembles the Micrsoft Office icons.
-      <div class="my-3"/> 
-      It takes the design language of Microsoft Office's icons and takes them a step further,
-      applying new ideas, colors and mechanics so that each icon is unique and special in their
-      own way. 
+  <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-slate-400">
+      I have <a class="font-semibold text-black dark:text-slate-300">2 years</a> of experience in Photo Editing.
       <div class="my-3"/>
-      iOffice has 3 styles: <a class="text-gray-800 dark:text-slate-300 font-semibold">Light, Dark, and Glyph.</a> 
-      Each style has 100 icons each, totalling around 300 icons.
+      This includes the skills I've learned in school which started in AUgust of 2022, and includes periods
+      of time where I taught myself a multitude of methods in terms of editing photos.
+      <div class="my-3"/>
+      At this time, I am most experienced with editing photographs and other forms of still media in 
+      <a class="font-semibold text-gray-800 dark:text-slate-300">Pixelmator Pro</a>.
+      However I do have some experience in <a class="font-semibold text-gray-800 dark:text-slate-300">Adobe Photoshop or Lightroom</a> as well as Apple's stock Photos app.
     </div>
 </div>
 
-<div class="flex flex-col md:flex-row items-center justify-center px-7 md:px-20 mb-12">
-  <div class="flex-grow flex-col md:flex-row flex w-full max-w-[1230px]">
-  <a download href="src/assets/mii2.png" type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="md:w-[170px] h-12 md:h-10 text-center justify-center transition ease-in-out delay-50 hover:scale-105 rounded-md  text-white bg-blue-500 dark:bg-blue-700 hover:bg-blue-700 dark:hover:bg-blue-500 items-center flex">
-    <div class="font-semibold sm:font-medium">Download</div>
-  </a>
- <div class="my-2 md:my-0 md:mx-2"/>
-  <a href="https://discord.gg/WXcdtQK" type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="w-full md:w-[170px] h-12 md:h-10 text-center justify-center transition ease-in-out delay-50 hover:scale-105 rounded-md  text-black dark:text-white bg-slate-100 dark:bg-slate-700 hover:bg-slate-200 dark:hover:bg-slate-600 items-center flex">
-    <div class="font-semibold sm:font-medium">Check it out</div>
-  </a> 
+<div class="my-10"/>
+
+<div class="flex flex-row items-center justify-center px-7 md:px-20">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-black dark:text-white">
+  <path d="M3.375 3C2.339 3 1.5 3.84 1.5 4.875v.75c0 1.036.84 1.875 1.875 1.875h17.25c1.035 0 1.875-.84 1.875-1.875v-.75C22.5 3.839 21.66 3 20.625 3H3.375z" />
+  <path fill-rule="evenodd" d="M3.087 9l.54 9.176A3 3 0 006.62 21h10.757a3 3 0 002.995-2.824L20.913 9H3.087zm6.163 3.75A.75.75 0 0110 12h4a.75.75 0 010 1.5h-4a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
+</svg>
+  <div class="mx-1"/>
+  <div class="flex-grow max-w-[1199px] text-2xl font-semibold text-black dark:text-white">Designing
+  </div>
 </div>
+<div class="flex flex-row px-7 md:px-20 justify-center">
+  <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-slate-400">
+      I have <a class="font-semibold text-black dark:text-slate-300">3 1/2 years</a> of experience in 
+      Design.
+      <div class="my-3"/>
+      I most commonly utilize what I've learned in UI Design, such as creating software concepts, or 
+      Graphic Design, such as creating application icons or logos. However it is
+      not limited to just those two forms of general design.
+      <div class="my-3"/>
+      At this time, I am most experienced with UI Design in <a class="font-semibold text-gray-800 dark:text-slate-300">Sketch</a>.
+      However for other forms of design such as Icon Design or general Graphic Design, I use <a class="font-semibold text-gray-800 dark:text-slate-300">Pixelmator Pro</a>
+      to create said designs.
+    </div>
 </div>
+<div class="flex px-7 md:px-20">
+<hr class="flex-grow max-w-[1230px] h-1 mx-auto bg-slate-200 border-0 rounded my-10 dark:bg-slate-700">
+</div>
+
+<div class="flex flex-row items-center justify-center px-7 md:px-20">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-black dark:text-white">
+  <path fill-rule="evenodd" d="M4.848 2.771A49.144 49.144 0 0112 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 01-3.476.383.39.39 0 00-.297.17l-2.755 4.133a.75.75 0 01-1.248 0l-2.755-4.133a.39.39 0 00-.297-.17 48.9 48.9 0 01-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97z" clip-rule="evenodd" />
+</svg>
+  <div class="mx-1"/>
+  <div class="flex-grow max-w-[1199px] text-2xl font-semibold text-black dark:text-white">My Contributions
+  </div>
+</div>
+<div class="flex flex-row px-7 md:px-20 justify-center mb-12">
+  <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-slate-400">
+      Below is a list of some of my well known contributions I have made in a plethora of situations. 
+      The more recent contributions are found higher on the list, with the newest or ongoing one being at the top.
+    </div>
+</div>
+<div class="flex flex-row items-center justify-center px-7 md:px-20">
+        <div class="items-center flex flex-row flex-grow max-w-[1230px] text-xl font-semibold text-black dark:text-white">      
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="w-5 h-5 mx-1 text-black dark:text-white">
+        <path fill="currentColor" d="M524.531,69.836a1.5,1.5,0,0,0-.764-.7A485.065,485.065,0,0,0,404.081,32.03a1.816,1.816,0,0,0-1.923.91,337.461,337.461,0,0,0-14.9,30.6,447.848,447.848,0,0,0-134.426,0,309.541,309.541,0,0,0-15.135-30.6,1.89,1.89,0,0,0-1.924-.91A483.689,483.689,0,0,0,116.085,69.137a1.712,1.712,0,0,0-.788.676C39.068,183.651,18.186,294.69,28.43,404.354a2.016,2.016,0,0,0,.765,1.375A487.666,487.666,0,0,0,176.02,479.918a1.9,1.9,0,0,0,2.063-.676A348.2,348.2,0,0,0,208.12,430.4a1.86,1.86,0,0,0-1.019-2.588,321.173,321.173,0,0,1-45.868-21.853,1.885,1.885,0,0,1-.185-3.126c3.082-2.309,6.166-4.711,9.109-7.137a1.819,1.819,0,0,1,1.9-.256c96.229,43.917,200.41,43.917,295.5,0a1.812,1.812,0,0,1,1.924.233c2.944,2.426,6.027,4.851,9.132,7.16a1.884,1.884,0,0,1-.162,3.126,301.407,301.407,0,0,1-45.89,21.83,1.875,1.875,0,0,0-1,2.611,391.055,391.055,0,0,0,30.014,48.815,1.864,1.864,0,0,0,2.063.7A486.048,486.048,0,0,0,610.7,405.729a1.882,1.882,0,0,0,.765-1.352C623.729,277.594,590.933,167.465,524.531,69.836ZM222.491,337.58c-28.972,0-52.844-26.587-52.844-59.239S193.056,219.1,222.491,219.1c29.665,0,53.306,26.82,52.843,59.239C275.334,310.993,251.924,337.58,222.491,337.58Zm195.38,0c-28.971,0-52.843-26.587-52.843-59.239S388.437,219.1,417.871,219.1c29.667,0,53.307,26.82,52.844,59.239C470.715,310.993,447.538,337.58,417.871,337.58Z"/>
+        </svg>
+        <div class="align-left text-xl font-semibold text-black dark:text-white">Header</div>
+      </div>
+ </div>
+ <div class="flex flex-row px-7 md:px-20 justify-center">
+   <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-slate-400 mb-4">
+       Paragraph here. Describing what exactly I contributed in this option. It'll be quick and brief only discussing
+       important and or relevant information. Could be long, or could be short. It depends on what exactly this contribution
+       is when decided at a later date. For now, it's a placeholder.
+     </div>
+ </div>
+
+ <div class="flex flex-row items-center justify-center px-7 md:px-20 mb-12">
+   <div class="flex-grow max-w-[1230px]">
+   <a href="https://discord.gg/WXcdtQK" type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="md:w-[170px] h-12 md:h-10 text-center justify-center transition ease-in-out delay-50 hover:scale-105 rounded-md  text-black dark:text-white bg-slate-100 dark:bg-slate-700 hover:bg-blue-400 dark:hover:bg-blue-600 hover:text-white items-center flex">
+     <div class="font-semibold sm:font-medium">Clicky Button</div>
+     </a>
+ </div> 
+ </div>
+
+ <div class="flex flex-row items-center justify-center px-7 md:px-20">
+        <div class="items-center flex flex-row flex-grow max-w-[1230px] text-xl font-semibold text-black dark:text-white">      
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="w-5 h-5 mx-1 text-black dark:text-white">
+        <path fill="currentColor" d="M524.531,69.836a1.5,1.5,0,0,0-.764-.7A485.065,485.065,0,0,0,404.081,32.03a1.816,1.816,0,0,0-1.923.91,337.461,337.461,0,0,0-14.9,30.6,447.848,447.848,0,0,0-134.426,0,309.541,309.541,0,0,0-15.135-30.6,1.89,1.89,0,0,0-1.924-.91A483.689,483.689,0,0,0,116.085,69.137a1.712,1.712,0,0,0-.788.676C39.068,183.651,18.186,294.69,28.43,404.354a2.016,2.016,0,0,0,.765,1.375A487.666,487.666,0,0,0,176.02,479.918a1.9,1.9,0,0,0,2.063-.676A348.2,348.2,0,0,0,208.12,430.4a1.86,1.86,0,0,0-1.019-2.588,321.173,321.173,0,0,1-45.868-21.853,1.885,1.885,0,0,1-.185-3.126c3.082-2.309,6.166-4.711,9.109-7.137a1.819,1.819,0,0,1,1.9-.256c96.229,43.917,200.41,43.917,295.5,0a1.812,1.812,0,0,1,1.924.233c2.944,2.426,6.027,4.851,9.132,7.16a1.884,1.884,0,0,1-.162,3.126,301.407,301.407,0,0,1-45.89,21.83,1.875,1.875,0,0,0-1,2.611,391.055,391.055,0,0,0,30.014,48.815,1.864,1.864,0,0,0,2.063.7A486.048,486.048,0,0,0,610.7,405.729a1.882,1.882,0,0,0,.765-1.352C623.729,277.594,590.933,167.465,524.531,69.836ZM222.491,337.58c-28.972,0-52.844-26.587-52.844-59.239S193.056,219.1,222.491,219.1c29.665,0,53.306,26.82,52.843,59.239C275.334,310.993,251.924,337.58,222.491,337.58Zm195.38,0c-28.971,0-52.843-26.587-52.843-59.239S388.437,219.1,417.871,219.1c29.667,0,53.307,26.82,52.844,59.239C470.715,310.993,447.538,337.58,417.871,337.58Z"/>
+        </svg>
+        <div class="align-left text-xl font-semibold text-black dark:text-white">Header</div>
+      </div>
+ </div>
+ <div class="flex flex-row px-7 md:px-20 justify-center">
+   <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-slate-400 mb-4">
+    Paragraph here. Describing what exactly I contributed in this option. It'll be quick and brief only discussing
+       important and or relevant information. Could be long, or could be short. It depends on what exactly this contribution
+       is when decided at a later date. For now, it's a placeholder.
+     </div>
+ </div>
+
+ <div class="flex flex-row items-center justify-center px-7 md:px-20 mb-12">
+   <div class="flex-grow max-w-[1230px]">
+   <a href="https://discord.gg/WXcdtQK" type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="md:w-[170px] h-12 md:h-10 text-center justify-center transition ease-in-out delay-50 hover:scale-105 rounded-md  text-black dark:text-white bg-slate-100 dark:bg-slate-700 hover:bg-blue-400 dark:hover:bg-blue-600 hover:text-white items-center flex">
+     <div class="font-semibold sm:font-medium">Clicky Button</div>
+     </a>
+ </div> 
+ </div>
+
 </div>
 </template>
 <script>
