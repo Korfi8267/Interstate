98f747d6e5d5706be3d29851c9926107817d636e
Korfi8267 <macwells3599@yahoo.com>
2023-03-26 16:43:26 -0500

    ADD: Affiliations category, M.B.S. affiliation

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -122,6 +122,48 @@
      </a>
  </div> 
  </div>
+<div class="flex px-7 md:px-20">
+<hr class="flex-grow max-w-[1230px] h-1 mx-auto bg-slate-200 border-0 rounded mb-10 dark:bg-slate-700">
+</div>
+
+<div class="flex flex-row items-center justify-center px-7 md:px-20">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-black dark:text-white">
+  <path fill-rule="evenodd" d="M4.848 2.771A49.144 49.144 0 0112 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 01-3.476.383.39.39 0 00-.297.17l-2.755 4.133a.75.75 0 01-1.248 0l-2.755-4.133a.39.39 0 00-.297-.17 48.9 48.9 0 01-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97z" clip-rule="evenodd" />
+</svg>
+  <div class="mx-1"/>
+  <div class="flex-grow max-w-[1199px] text-2xl font-semibold text-black dark:text-white">My Affiliations
+  </div>
+</div>
+<div class="flex flex-row px-7 md:px-20 justify-center mb-12">
+  <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-slate-400">
+      Below is a list of official affiliations I am involved with, such as groups, or companies, if that ever happens.
+      They will over time be updated depending on what happens, so pelase do check every now and then.
+    </div>
+</div>
+
+<div class="flex flex-row items-center justify-center px-7 md:px-20">
+        <div class="items-center flex flex-row flex-grow max-w-[1230px] text-xl font-semibold text-black dark:text-white">      
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="w-5 h-5 mx-1 text-black dark:text-white">
+        <path fill="currentColor" d="M524.531,69.836a1.5,1.5,0,0,0-.764-.7A485.065,485.065,0,0,0,404.081,32.03a1.816,1.816,0,0,0-1.923.91,337.461,337.461,0,0,0-14.9,30.6,447.848,447.848,0,0,0-134.426,0,309.541,309.541,0,0,0-15.135-30.6,1.89,1.89,0,0,0-1.924-.91A483.689,483.689,0,0,0,116.085,69.137a1.712,1.712,0,0,0-.788.676C39.068,183.651,18.186,294.69,28.43,404.354a2.016,2.016,0,0,0,.765,1.375A487.666,487.666,0,0,0,176.02,479.918a1.9,1.9,0,0,0,2.063-.676A348.2,348.2,0,0,0,208.12,430.4a1.86,1.86,0,0,0-1.019-2.588,321.173,321.173,0,0,1-45.868-21.853,1.885,1.885,0,0,1-.185-3.126c3.082-2.309,6.166-4.711,9.109-7.137a1.819,1.819,0,0,1,1.9-.256c96.229,43.917,200.41,43.917,295.5,0a1.812,1.812,0,0,1,1.924.233c2.944,2.426,6.027,4.851,9.132,7.16a1.884,1.884,0,0,1-.162,3.126,301.407,301.407,0,0,1-45.89,21.83,1.875,1.875,0,0,0-1,2.611,391.055,391.055,0,0,0,30.014,48.815,1.864,1.864,0,0,0,2.063.7A486.048,486.048,0,0,0,610.7,405.729a1.882,1.882,0,0,0,.765-1.352C623.729,277.594,590.933,167.465,524.531,69.836ZM222.491,337.58c-28.972,0-52.844-26.587-52.844-59.239S193.056,219.1,222.491,219.1c29.665,0,53.306,26.82,52.843,59.239C275.334,310.993,251.924,337.58,222.491,337.58Zm195.38,0c-28.971,0-52.843-26.587-52.843-59.239S388.437,219.1,417.871,219.1c29.667,0,53.307,26.82,52.844,59.239C470.715,310.993,447.538,337.58,417.871,337.58Z"/>
+        </svg>
+        <div class="align-left text-xl font-semibold text-black dark:text-white">M.B.S.</div>
+      </div>
+ </div>
+ <div class="flex flex-row px-7 md:px-20 justify-center">
+   <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-slate-400 mb-4">
+    I am a known member and administrator of the <a class="font-semibold text-gray-800 dark:text-slate-300">M.B.S. Variety Gaming</a> group.
+    This group primarily focuses on gaming, and bringing the fun back into the gaming industry. It is a small humble group, but we are always
+    open for new members.
+     </div>
+ </div>
+
+ <div class="flex flex-row items-center justify-center px-7 md:px-20 mb-12">
+   <div class="flex-grow max-w-[1230px]">
+   <a href="https://guilded.gg/MBS-Variety-Gaming" type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="md:w-[170px] h-12 md:h-10 text-center justify-center transition ease-in-out delay-50 hover:scale-105 rounded-md  text-black dark:text-white bg-slate-100 dark:bg-slate-700 hover:bg-blue-400 dark:hover:bg-blue-600 hover:text-white items-center flex">
+     <div class="font-semibold sm:font-medium">Join the Fun</div>
+     </a>
+ </div> 
+ </div>
 
 </div>
 </template>
