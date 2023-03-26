92500bb9c2001e4bf0f046940b950ddf04b179ae
Korfi8267 <macwells3599@yahoo.com>
2023-03-26 12:11:20 -0500

    RMV: Horizonal list ADD: iOffice template PKG info

--- src/views/Packages.vue
+++ src/views/Packages.vue
@@ -1,52 +1,48 @@
 <template>
 <div class="bg-white dark:bg-slate-900 scroll-smooth overflow-auto">
-<div class="my-12"/>
+<div class="my-24"/>
 
-<div class="text-center font-bold text-5xl text-black dark:text-white my-2">You're early.</div>
-<div class="text-center font-medium text-3xl text-slate-700 dark:text-slate-400 mb-16">Check back later.</div>
-<div class="text-center font-medium text-3xl text-slate-700 dark:text-slate-400 mb-2">Here's a Mii.</div>
-<img class="mx-auto align-center rounded-full w-32 h-32 transition ease-in-out bg-blue-100 hover:bg-blue-200 dark:bg-blue-900 dark:hover:bg-blue-800 mb-12" src="../assets/mii2.png" />
+<div class="flex flex-col md:flex-row items-center text-center md:text-left justify-center px-7 md:px-20">
+    <img class="w-16 md:w-12 h-16 md:h-12" src="../assets/box.png" /><div class="px-4 py-4"></div>
+    <div class="text-3xl font-semibold text-black dark:text-white">My Packages.<div class="text-xl font-normal text-gray-600 dark:text-gray-300">Items that are available for download.</div></div>
+    <div class="py-4 md:py-0"></div><div class="flex grow max-w-[824px]"></div>
+  </div>
 
-<div class="flex flex-nowrap justify-center">
-  <div class="w-[1230px] h-96 flex-row flex-nowrap flex snap-mandatory snap-x scroll-smooth overflow-x-scroll gap-14 px-7 md:px-20 mb-12">
-    <div class="scroll-ml-7 snap-start w-72 h-96 bg-blue-500 rounded-md p-3 flex flex-row shrink-0 items-end">
-      <div class="w-32 h-10 bg-green-500 rounded-md text-white font-semibold">Title</div>
-      <div class="flex-grow"></div>
-      <div class="w-10 h-10 bg-red-500 rounded-md text-white font-semibold">DL</div>
-    </div>
+<div class="my-[88px] md:my-24"/>
 
-    <div class="scroll-ml-7 snap-start w-72 h-96 bg-blue-500 rounded-md p-3 flex flex-row shrink-0 items-end">
-      <div class="w-32 h-10 bg-green-500 rounded-md text-white font-semibold">Title</div>
-      <div class="flex-grow"></div>
-      <div class="w-10 h-10 bg-red-500 rounded-md text-white font-semibold">DL</div>
-    </div>
-
-    <div class="scroll-ml-7 snap-start w-72 h-96 bg-blue-500 rounded-md p-3 flex flex-row shrink-0 items-end">
-      <div class="w-32 h-10 bg-green-500 rounded-md text-white font-semibold">Title</div>
-      <div class="flex-grow"></div>
-      <div class="w-10 h-10 bg-red-500 rounded-md text-white font-semibold">DL</div>
-    </div>
-
-    <div class="scroll-ml-7 snap-start w-72 h-96 bg-blue-500 rounded-md p-3 flex flex-row shrink-0 items-end">
-      <div class="w-32 h-10 bg-green-500 rounded-md text-white font-semibold">Title</div>
-      <div class="flex-grow"></div>
-      <div class="w-10 h-10 bg-red-500 rounded-md text-white font-semibold">DL</div>
-    </div>
-
-    <div class="scroll-ml-7 snap-start w-72 h-96 bg-blue-500 rounded-md p-3 flex flex-row shrink-0 items-end">
-      <div class="w-32 h-10 bg-green-500 rounded-md text-white font-semibold">Title</div>
-      <div class="flex-grow"></div>
-      <div class="w-10 h-10 bg-red-500 rounded-md text-white font-semibold">DL</div>
-    </div>
-
-    <div class="scroll-ml-7 snap-start w-72 h-96 bg-blue-500 rounded-md p-3 flex flex-row shrink-0 items-end">
-      <div class="w-32 h-10 bg-green-500 rounded-md text-white font-semibold">Title</div>
-      <div class="flex-grow"></div>
-      <div class="w-10 h-10 bg-red-500 rounded-md text-white font-semibold">DL</div>
-    </div>
+<div class="flex flex-row items-center justify-center px-7 md:px-20">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-black dark:text-white">
+  <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clip-rule="evenodd" />
+</svg>
+  <div class="mx-1"/>
+  <div class="flex-grow max-w-[1199px] text-2xl font-semibold text-black dark:text-white">
+    iOffice
   </div>
 </div>
+<div class="flex flex-row px-7 md:px-20 justify-center">
+  <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-slate-400 mb-4">
+      iOffice brings productivity to your home screen.
+      <div class="my-3"/> 
+      It takes the design language of Microsoft Office's icons and takes them a step further,
+      applying new ideas, colors and mechanics so that each icon is unique and special in their
+      own way. 
+      <div class="my-3"/>
+      iOffice has 3 styles: <a class="text-gray-800 dark:text-slate-300 font-semibold">Light, Dark, and Glyph.</a> 
+      Each style has 100 icons each, totalling around 300 icons.
+    </div>
+</div>
 
+<div class="flex flex-col md:flex-row items-center justify-center px-7 md:px-20 mb-12">
+  <div class="flex-grow flex-col md:flex-row flex w-full max-w-[1230px]">
+  <a download href="src/assets/mii2.png" type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="md:w-[170px] h-12 md:h-10 text-center justify-center transition ease-in-out delay-50 hover:scale-105 rounded-md  text-white bg-blue-500 dark:bg-blue-700 hover:bg-blue-700 dark:hover:bg-blue-500 items-center flex">
+    <div class="font-semibold sm:font-medium">Download</div>
+  </a>
+ <div class="my-2 md:my-0 md:mx-2"/>
+  <a href="https://discord.gg/WXcdtQK" type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="w-full md:w-[170px] h-12 md:h-10 text-center justify-center transition ease-in-out delay-50 hover:scale-105 rounded-md  text-black dark:text-white bg-slate-100 dark:bg-slate-700 hover:bg-slate-200 dark:hover:bg-slate-600 items-center flex">
+    <div class="font-semibold sm:font-medium">Check it out</div>
+  </a> 
+</div>
+</div>
 </div>
 </template>
 <script>
