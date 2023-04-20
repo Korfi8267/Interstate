96cb5b80de817620286c2954ac2fc3d7c1f7f151
Korfi8267 <macwells3599@yahoo.com>
2023-04-19 20:29:02 -0500

    add watchapps contribution

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -78,6 +78,30 @@
 <div class="flex flex-row items-center justify-center px-7 md:px-20">
         <div class="items-center flex flex-row flex-grow max-w-[1230px] text-xl font-semibold text-black dark:text-white">      
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 mx-1 text-black dark:text-white">
+            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.75-13a.75.75 0 00-1.5 0v5c0 .414.336.75.75.75h4a.75.75 0 000-1.5h-3.25V5z" clip-rule="evenodd" />
+          </svg>
+        <div class="align-left text-xl font-semibold text-black dark:text-white">WatchApps</div>
+      </div>
+ </div>
+ <div class="flex flex-row px-7 md:px-20 justify-center">
+   <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-slate-400 mb-4">
+       I have designed the official icon for a small business called WatchApps, along with other contributions,
+       which is ran by my friends <a class="font-semibold text-gray-800 dark:text-slate-300">llsc12 & SuperBro</a>.
+       They are a team that have created projects such as WatchTube, WatchMusic, and many more.
+     </div>
+ </div>
+
+ <div class="flex flex-row items-center justify-center px-7 md:px-20 mb-12">
+   <div class="flex-grow max-w-[1230px]">
+   <a href="https://watchapps.org" type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="md:w-[170px] h-12 md:h-10 text-center justify-center transition ease-in-out delay-50 hover:scale-105 rounded-md  text-black dark:text-white bg-slate-100 dark:bg-slate-700 hover:bg-blue-400 dark:hover:bg-blue-600 hover:text-white items-center flex">
+     <div class="font-semibold sm:font-medium">Check them Out</div>
+     </a>
+ </div> 
+ </div>
+
+ <div class="flex flex-row items-center justify-center px-7 md:px-20">
+        <div class="items-center flex flex-row flex-grow max-w-[1230px] text-xl font-semibold text-black dark:text-white">      
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 mx-1 text-black dark:text-white">
             <path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h9A1.5 1.5 0 0114 3.5v11.75A2.75 2.75 0 0016.75 18h-12A2.75 2.75 0 012 15.25V3.5zm3.75 7a.75.75 0 000 1.5h4.5a.75.75 0 000-1.5h-4.5zm0 3a.75.75 0 000 1.5h4.5a.75.75 0 000-1.5h-4.5zM5 5.75A.75.75 0 015.75 5h4.5a.75.75 0 01.75.75v2.5a.75.75 0 01-.75.75h-4.5A.75.75 0 015 8.25v-2.5z" clip-rule="evenodd" />
             <path d="M16.5 6.5h-1v8.75a1.25 1.25 0 102.5 0V8a1.5 1.5 0 00-1.5-1.5z" />
           </svg>
@@ -100,29 +124,6 @@
  </div> 
  </div>
 
- <div class="flex flex-row items-center justify-center px-7 md:px-20">
-        <div class="items-center flex flex-row flex-grow max-w-[1230px] text-xl font-semibold text-black dark:text-white">      
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="w-5 h-5 mx-1 text-black dark:text-white">
-        <path fill="currentColor" d="M524.531,69.836a1.5,1.5,0,0,0-.764-.7A485.065,485.065,0,0,0,404.081,32.03a1.816,1.816,0,0,0-1.923.91,337.461,337.461,0,0,0-14.9,30.6,447.848,447.848,0,0,0-134.426,0,309.541,309.541,0,0,0-15.135-30.6,1.89,1.89,0,0,0-1.924-.91A483.689,483.689,0,0,0,116.085,69.137a1.712,1.712,0,0,0-.788.676C39.068,183.651,18.186,294.69,28.43,404.354a2.016,2.016,0,0,0,.765,1.375A487.666,487.666,0,0,0,176.02,479.918a1.9,1.9,0,0,0,2.063-.676A348.2,348.2,0,0,0,208.12,430.4a1.86,1.86,0,0,0-1.019-2.588,321.173,321.173,0,0,1-45.868-21.853,1.885,1.885,0,0,1-.185-3.126c3.082-2.309,6.166-4.711,9.109-7.137a1.819,1.819,0,0,1,1.9-.256c96.229,43.917,200.41,43.917,295.5,0a1.812,1.812,0,0,1,1.924.233c2.944,2.426,6.027,4.851,9.132,7.16a1.884,1.884,0,0,1-.162,3.126,301.407,301.407,0,0,1-45.89,21.83,1.875,1.875,0,0,0-1,2.611,391.055,391.055,0,0,0,30.014,48.815,1.864,1.864,0,0,0,2.063.7A486.048,486.048,0,0,0,610.7,405.729a1.882,1.882,0,0,0,.765-1.352C623.729,277.594,590.933,167.465,524.531,69.836ZM222.491,337.58c-28.972,0-52.844-26.587-52.844-59.239S193.056,219.1,222.491,219.1c29.665,0,53.306,26.82,52.843,59.239C275.334,310.993,251.924,337.58,222.491,337.58Zm195.38,0c-28.971,0-52.843-26.587-52.843-59.239S388.437,219.1,417.871,219.1c29.667,0,53.307,26.82,52.844,59.239C470.715,310.993,447.538,337.58,417.871,337.58Z"/>
-        </svg>
-        <div class="align-left text-xl font-semibold text-black dark:text-white">Header</div>
-      </div>
- </div>
- <div class="flex flex-row px-7 md:px-20 justify-center">
-   <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-slate-400 mb-4">
-    Paragraph here. Describing what exactly I contributed in this option. It'll be quick and brief only discussing
-       important and or relevant information. Could be long, or could be short. It depends on what exactly this contribution
-       is when decided at a later date. For now, it's a placeholder.
-     </div>
- </div>
-
- <div class="flex flex-row items-center justify-center px-7 md:px-20 mb-12">
-   <div class="flex-grow max-w-[1230px]">
-   <a href="https://discord.gg/WXcdtQK" type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="md:w-[170px] h-12 md:h-10 text-center justify-center transition ease-in-out delay-50 hover:scale-105 rounded-md  text-black dark:text-white bg-slate-100 dark:bg-slate-700 hover:bg-blue-400 dark:hover:bg-blue-600 hover:text-white items-center flex">
-     <div class="font-semibold sm:font-medium">Clicky Button</div>
-     </a>
- </div> 
- </div>
 <div class="flex px-7 md:px-20">
 <hr class="flex-grow max-w-[1230px] h-1 mx-auto bg-slate-200 border-0 rounded mb-10 dark:bg-slate-700">
 </div>
