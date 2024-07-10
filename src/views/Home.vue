98bad7a639df7eb4273e2875f3f2f059599df13f
Korfi8267 <macwells3599@yahoo.com>
2024-07-10 13:28:24 -0500

    Update Header of Home Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,11 @@
 <template>
-<div class="bg-white dark:bg-slate-900 scroll-smooth overflow-auto">
+<div class="bg-white dark:bg-neutral-900 scroll-smooth overflow-auto pb-24 md:pb-0">
 <div class="my-24"></div>
 
-<div class="flex flex-col md:flex-row items-center text-center md:text-left justify-center px-7 md:px-20">
-  <img class="w-16 md:w-12 h-16 md:h-12 transition ease-in-out delay-50 hover:rotate-12" src="../assets/handwave.png" /><div class="px-4 py-4"></div>
-  <div class="text-3xl font-semibold text-black dark:text-white">Hi there.<div class="text-xl font-normal text-gray-600 dark:text-gray-300">I'm a designer and photographer.</div></div>
-  <div class="py-4 md:py-0"></div><div class="flex grow max-w-[700px]"></div>
-  <div class="box-border h-10 w-40 transition ease-in-out delay-50 hover:scale-105 bg-slate-100 dark:bg-slate-700 hover:bg-blue-400 dark:hover:bg-blue-600 rounded-md">
-    <button class="transition ease-in-out delay-50 text-black dark:text-white hover:text-white hover:font-semibold mx-auto px-5 h-10 w-40"><router-link to="/Portfolio">View Portfolio</router-link></button>
-  </div>
+<div class="flex flex-col items-center justify-center px-7 md:px-2">
+  <div class="w-48 h-48 rounded-full bg-blue-500"></div>
+  <span class=" pt-12 font-black text-6xl text-black dark:text-white">Welcome.</span>
+  <span class="text-center pt-4 max-w-[480px] font-medium text-xl text-gray-600 dark:text-neutral-400">This is my personal website, proudly coded from scratch in Vue & Tailwind. Here, you can learn who I am, & check out my work, & contact me if need be.</span>
 </div>
 
 <div class="my-12 md:my-24"/>
@@ -23,16 +20,15 @@
   </div>
 </div>
 <div class="flex flex-row px-7 md:px-20 justify-center">
-  <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-slate-400">
+  <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-neutral-400">
       I am Mac, otherwise known as Korfi online.
       <div class="my-3"/> 
-      I am a graphic designer and amatuer photographer and media editor from Oklahoma. I am attending a 
-      school for a Multimedia class where I am learning image and video editing, coding, graphical 
-      design and more. 
+      I am a graphic designer and amatuer photographer and media editor from Oklahoma. I am a graduate from a
+      Multimedia class where I learned image, audio and video editing, coding, graphical design and more. 
       <div class="my-3"/>
-      I have roughly <a class="text-gray-800 dark:text-slate-300 font-semibold">2 1/2 years</a> 
-      of experience designing software concepts. I alays love to tinker with design, as
-      well as experiment with new ideas pushing the limits of the software we have today.
+      I have roughly <a class="text-gray-800 dark:text-gray-300 font-semibold">2 1/2 years</a> 
+      of experience designing software concepts. I alays love to tinker with design & technology, as
+      well as experiment with new ideas pushing the limits of the software & hardware we have today.
     </div>
 </div>
 
@@ -48,139 +44,44 @@
   </div>
 </div>
 <div class="flex flex-row px-7 md:px-20 justify-center">
-  <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-slate-400">
-      My most notable project is <a class="font-semibold text-black dark:text-slate-300">iOffice</a>.
+  <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-neutral-400">
+      My most notable project is <a class="font-semibold text-black dark:text-gray-300">iOffice</a>.
       <div class="my-3"/>
       However I have created a multitude of other projects, usually revolving around designs
       and concepts. Most revolve around Apple's iOS, iPadOS and macOS softwares respectively.
       <div class="my-3"/>
       I have also collaborated with a multitude of designers and developers, including 
-      <a class="font-semibold text-gray-800 dark:text-slate-300">llsc12, Paras KCD, and more.</a>
+      <a class="font-semibold text-gray-800 dark:text-gray-300">llsc12, Paras KCD, and more.</a>
       Special thanks to everyone who helped get me this far!
     </div>
 </div>
 
-<div class="flex px-7 md:px-20">
-<hr class="flex-grow max-w-[1230px] h-1 mx-auto bg-slate-200 border-0 rounded my-10 dark:bg-slate-700">
+<div class="flex flex-row items-center justify-left px-7 md:px-20">
+<ul role="list" class="marker:text-lg marker:text-blue-500 marker:dark:text-blue-700 list-disc pl-5 space-y-3 text-gray-400">
+  <li>5 cups chopped Porcini mushrooms</li>
+  <li>1/2 cup of olive oil</li>
+  <li>3lb of celery</li>
+</ul>
 </div>
-
-<div class="flex flex-row items-center justify-center px-7 md:px-20">
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-black dark:text-white">
-  <path fill-rule="evenodd" d="M4.848 2.771A49.144 49.144 0 0112 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 01-3.476.383.39.39 0 00-.297.17l-2.755 4.133a.75.75 0 01-1.248 0l-2.755-4.133a.39.39 0 00-.297-.17 48.9 48.9 0 01-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97z" clip-rule="evenodd" />
-</svg>
-  <div class="mx-1"/>
-  <div class="flex-grow max-w-[1199px] text-2xl font-semibold text-black dark:text-white">My Contacts
-  </div>
-</div>
-<div class="flex flex-row px-7 md:px-20 justify-center mb-12">
-  <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-slate-400">
-      Below you can access any method of contact I have added thus far. These may change often, 
-      so please check every now and then. Please choose from the options available depending on
-      your reason of contact.
+<div class="max-w-lg mx-auto p-8">
+  <details class="open:bg-white dark:open:bg-slate-900 open:ring-1 open:ring-black/5 dark:open:ring-white/10 open:shadow-lg p-6 rounded-lg" open>
+    <summary class="text-sm leading-6 text-slate-900 dark:text-white font-semibold select-none">
+      Why do they call it Ovaltine?
+    </summary>
+    <div class="mt-3 text-sm leading-6 text-slate-600 dark:text-slate-400">
+      <p>The mug is round. The jar is round. They should call it Roundtine.</p>
     </div>
+  </details>
 </div>
 
-<div class="flex flex-row items-center justify-center px-7 md:px-20">
-        <div class="items-center flex flex-row flex-grow max-w-[1230px] text-xl font-semibold text-black dark:text-white">      
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="w-5 h-5 mx-1 text-black dark:text-white">
-        <path fill="currentColor" d="M524.531,69.836a1.5,1.5,0,0,0-.764-.7A485.065,485.065,0,0,0,404.081,32.03a1.816,1.816,0,0,0-1.923.91,337.461,337.461,0,0,0-14.9,30.6,447.848,447.848,0,0,0-134.426,0,309.541,309.541,0,0,0-15.135-30.6,1.89,1.89,0,0,0-1.924-.91A483.689,483.689,0,0,0,116.085,69.137a1.712,1.712,0,0,0-.788.676C39.068,183.651,18.186,294.69,28.43,404.354a2.016,2.016,0,0,0,.765,1.375A487.666,487.666,0,0,0,176.02,479.918a1.9,1.9,0,0,0,2.063-.676A348.2,348.2,0,0,0,208.12,430.4a1.86,1.86,0,0,0-1.019-2.588,321.173,321.173,0,0,1-45.868-21.853,1.885,1.885,0,0,1-.185-3.126c3.082-2.309,6.166-4.711,9.109-7.137a1.819,1.819,0,0,1,1.9-.256c96.229,43.917,200.41,43.917,295.5,0a1.812,1.812,0,0,1,1.924.233c2.944,2.426,6.027,4.851,9.132,7.16a1.884,1.884,0,0,1-.162,3.126,301.407,301.407,0,0,1-45.89,21.83,1.875,1.875,0,0,0-1,2.611,391.055,391.055,0,0,0,30.014,48.815,1.864,1.864,0,0,0,2.063.7A486.048,486.048,0,0,0,610.7,405.729a1.882,1.882,0,0,0,.765-1.352C623.729,277.594,590.933,167.465,524.531,69.836ZM222.491,337.58c-28.972,0-52.844-26.587-52.844-59.239S193.056,219.1,222.491,219.1c29.665,0,53.306,26.82,52.843,59.239C275.334,310.993,251.924,337.58,222.491,337.58Zm195.38,0c-28.971,0-52.843-26.587-52.843-59.239S388.437,219.1,417.871,219.1c29.667,0,53.307,26.82,52.844,59.239C470.715,310.993,447.538,337.58,417.871,337.58Z"/>
-        </svg>
-        <div class="align-left text-xl font-semibold text-black dark:text-white">Discord</div>
-      </div>
- </div>
- <div class="flex flex-row px-7 md:px-20 justify-center">
-   <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-slate-400 mb-4">
-       I primarily use Discord for <a class="font-semibold text-gray-800 dark:text-slate-300">personal contact</a>, 
-       however I also use it for providing updates and releases for current and upcoming projects. Feel free to join
-       to stay up to date. 
-     </div>
- </div>
+<div class="my-32"/>
 
- <div class="flex flex-row items-center justify-center px-7 md:px-20 mb-12">
-   <div class="flex-grow max-w-[1230px]">
-   <a href="https://discord.gg/WXcdtQK" type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="md:w-[170px] h-12 md:h-10 text-center justify-center transition ease-in-out delay-50 hover:scale-105 rounded-md  text-black dark:text-white bg-slate-100 dark:bg-slate-700 hover:bg-blue-400 dark:hover:bg-blue-600 hover:text-white items-center flex">
-     <div class="font-semibold sm:font-medium">Open Discord</div>
-     </a>
- </div> 
- </div>
-
- <div class="flex flex-row items-center justify-center px-7 md:px-20">
-        <div class="items-center flex flex-row flex-grow max-w-[1230px] text-xl font-semibold text-black dark:text-white">      
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 mx-1 text-black dark:text-white">
-  <path fill-rule="evenodd" d="M8.25 6.75a3.75 3.75 0 117.5 0 3.75 3.75 0 01-7.5 0zM15.75 9.75a3 3 0 116 0 3 3 0 01-6 0zM2.25 9.75a3 3 0 116 0 3 3 0 01-6 0zM6.31 15.117A6.745 6.745 0 0112 12a6.745 6.745 0 016.709 7.498.75.75 0 01-.372.568A12.696 12.696 0 0112 21.75c-2.305 0-4.47-.612-6.337-1.684a.75.75 0 01-.372-.568 6.787 6.787 0 011.019-4.38z" clip-rule="evenodd" />
-  <path d="M5.082 14.254a8.287 8.287 0 00-1.308 5.135 9.687 9.687 0 01-1.764-.44l-.115-.04a.563.563 0 01-.373-.487l-.01-.121a3.75 3.75 0 013.57-4.047zM20.226 19.389a8.287 8.287 0 00-1.308-5.135 3.75 3.75 0 013.57 4.047l-.01.121a.563.563 0 01-.373.486l-.115.04c-.567.2-1.156.349-1.764.441z" />
-</svg>
-
-        <div class="align-left text-xl font-semibold text-black dark:text-white">Guilded</div>
-      </div>
- </div>
- <div class="flex flex-row px-7 md:px-20 justify-center">
-   <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-slate-400 mb-4">
-       I have recently switched over to Guilded <a class="font-semibold text-gray-800 dark:text-slate-300">from Discord</a>, 
-       and I am heavily considering staying there. However, for the time being, my Guilded server will be up along with my Discord
-       server.
-     </div>
- </div>
-
- <div class="flex flex-row items-center justify-center px-7 md:px-20 mb-12">
-   <div class="flex-grow max-w-[1230px]">
-   <a href="https://guilded.gg/The-Interstate" type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="md:w-[170px] h-12 md:h-10 text-center justify-center transition ease-in-out delay-50 hover:scale-105 rounded-md  text-black dark:text-white bg-slate-100 dark:bg-slate-700 hover:bg-blue-400 dark:hover:bg-blue-600 hover:text-white items-center flex">
-     <div class="font-semibold sm:font-medium">Open Guilded</div>
-     </a>
- </div> 
- </div>
-
- <div class="flex flex-row items-center justify-center px-7 md:px-20">
-        <div class="items-center flex flex-row flex-grow max-w-[1230px] text-xl font-semibold text-black dark:text-white">      
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 h-5 mx-1 text-black dark:text-white">
-          <path fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/>
-          </svg>
-        <div class="align-left text-xl font-semibold text-black dark:text-white">GitHub</div>
-      </div>
- </div>
- <div class="flex flex-row px-7 md:px-20 justify-center">
-   <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-slate-400 mb-4">
-       GitHub is the place where I upload my <a class="font-semibold text-gray-800 dark:text-slate-300">open-source projects</a>, 
-       such as this website. Issues that revolve around anything found on my github profile should be brought up in the issues tab for the relevant repository.
-     </div>
- </div>
- <div class="flex flex-row items-center justify-center px-7 md:px-20 mb-12">
-   <div class="flex-grow max-w-[1230px]">
-     <a href="https://github.com/Korfi8267" type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="md:w-[170px] h-12 md:h-10 text-center justify-center transition ease-in-out delay-50 hover:scale-105 rounded-md  text-black dark:text-white bg-slate-100 dark:bg-slate-700 hover:bg-blue-400 dark:hover:bg-blue-600 hover:text-white items-center flex">
-   <div class="font-semibold sm:font-medium">Open Github</div>
-     </a>
-   </div>
- </div>
- <div class="flex flex-row items-center justify-center px-7 md:px-20">
-        <div class="items-center flex flex-row flex-grow max-w-[1230px] text-xl font-semibold text-black dark:text-white">      
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 h-5 mx-1 text-black dark:text-white">
-      <path fill="currentColor" d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"/>
-    </svg>
-        <div class="align-left text-xl font-semibold text-black dark:text-white">Twitter</div>
-      </div>
- </div>
- <div class="flex flex-row px-7 md:px-20 justify-center">
-   <div class="flex-grow max-w-[1230px] text-lg font-medium text-slate-600 dark:text-slate-400 mb-4">
-       I use Twitter almost exclusively on a <a class="font-semibold text-gray-800 dark:text-slate-300">personal basis</a>, 
-       but sometimes I do announce projects that are almost complete there; though that isn't guaranteed. Follow to stay up to date.
-     </div>
- </div>
-
- <div class="flex flex-row items-center justify-center px-7 md:px-20">
-   <div class="flex-grow max-w-[1230px]">
-     <a href="https://twitter.com/Korfi8267" type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="md:w-[170px] h-12 md:h-10 text-center justify-center transition ease-in-out delay-50 hover:scale-105 rounded-md text-black dark:text-white bg-slate-100 dark:bg-slate-700 hover:bg-blue-400 dark:hover:bg-blue-600 hover:text-white items-center flex">
-     <div class="font-semibold sm:font-medium">Open Twitter</div>
-     </a>
- </div>
-</div>
-<div class="my-16"/>
-
-<div class="text-center font-regular text-blue-400 dark:text-blue-500">
+<div class="text-center font-regular text-blue-400 dark:text-slate-500">
   <span class="text-lg">Created by <a class="font-medium text-blue-600 dark:text-white">Korfi</a> in Oklahoma</span>
 </div>
 
-<div class="pt-2 text-center font-regular text-gray-400 dark:text-blue-500/70">
-  <span class="text-sm">Copyright <a class="font-bold text-gray-600 dark:text-blue-200">Korfi, 2023</a>. All rights reserved.</span>
+<div class="pt-2 text-center font-regular text-gray-400 dark:text-slate-500/70">
+  <span class="text-sm">Copyright <a class="font-bold text-gray-600 dark:text-slate-200/50">Korfi, 2023-2024</a>. All rights reserved.</span>
 </div>
 
 <div class="my-12"></div>
